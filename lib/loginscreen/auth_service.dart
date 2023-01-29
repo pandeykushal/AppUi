b811b7188f510dadc8c686b252114355684408cf
KUSHAL PANDEY <kUSHAL.PANDEY118@GMAIL.COM>
2023-01-29 13:10:53 +0545

    firebase login working

---
+++ lib/loginscreen/auth_service.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'homescreen.dart';
+import 'login.dart';
+
+class AuthService{
+ 
+
+  //Determine if the user is authenticated.
+  handleAuthState() {
+    return StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return const LoginPage();
+          }
+        });
+  }
+
+  void signInWithGoogle() {}
+
+  void signOut() {}
+}
+ signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+        scopes: <String>["email"]).signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+  //Sign out
+  signOut() {
+    FirebaseAuth.instance.signOut();
+  }
